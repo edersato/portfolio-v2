216795f3e204f269d399e3075c6dbbd6269066ed
edersato <edersato@gmail.com>
2023-08-07 14:48:54 -0300

    setting base project

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HomePage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage
   }
 }
 </script>
 
-<style>
-#app {
+<style lang="scss">
+html {
+  background: #242122;
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+}
+
+#app {
+  background: #242122;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+
+/* Firefox */
+* {
+  scrollbar-width: thin;
+  scrollbar-color: #4591C4 #242122;
+}
+
+/* Chrome, Edge and Safari */
+*::-webkit-scrollbar {
+  width: 10px;
+  width: 10px;
+
+  &::-webkit-scrollbar-track {
+    border-radius: 4px;
+    background-color: #242122;
+  
+    &:hover {
+      background-color: #242122;
+    }
+  
+    &:active {
+      background-color: #242122;
+    }
+  }
+}
+
+*::-webkit-scrollbar-thumb {
+  border-radius: 5px;
+  background-color: #4591C4;
+
+  &:hover {
+    background-color: #306589;
+  }
+
+  &:active {
+    background-color: #4591C4;
+  }
 }
 </style>
