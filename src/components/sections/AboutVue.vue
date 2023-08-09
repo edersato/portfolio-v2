432a96902895c1464cce2e9aa59964fbde494d19
edersato <edersato@gmail.com>
2023-08-09 19:42:14 -0300

    projects area, need fix and media queries

--- src/components/sections/AboutVue.vue
+++ src/components/sections/AboutVue.vue
@@ -8,10 +8,10 @@
       <div>
         <h4 class="mb-3">Quem sou?</h4>
         <p>
-          Sou um <b>Desenvolvedor Front End</b> com 2 anos de experiência com
+          Sou <b>Desenvolvedor Front End</b> com 2 anos de experiência com
           foco em <b>webdesign</b>, onde aplico todo meu conhecimento para
-          extrair o melhor do produto que estou desenvolvendo. Confira na seção
-          de <b>Projetos</b> mais detalhes.
+          extrair o melhor do produto que estou desenvolvendo. <br> 
+          Confira na seção de <b>Projetos</b> mais detalhes.
         </p>
 
         <p>
@@ -28,7 +28,7 @@
         <p>
           Estou aberto a novas <b>oportunidades de trabalho</b> que possa
           contribuir, crescer e aprender mais. Se você possui uma vaga que meus
-          conhecimentos agregem seu time, não hesite de entrar em
+          conhecimentos agreguem seu time, não hesite de entrar em
           <b>contato</b>.
         </p>
       </div>
@@ -53,7 +53,7 @@ export default {
 .aboutSection {
   display: grid;
   grid-template-columns: 2fr 1fr;
-  grid-column-gap: 2em;
+  grid-column-gap: 3em;
   margin: 0 5em;
 
   @media (min-width: 320px) and (max-width: 920px) {
@@ -64,9 +64,9 @@ export default {
   p {
     color: white;
     text-align: left;
-    line-height: 2rem;
+    line-height: 1.2rem;
     font-size: 1.1rem;
-    padding: 1em 0;
+    padding: 1em;
 
       @media (min-width: 320px) and (max-width: 768px) {
         font-size: 1rem;
--- src/components/sections/ProjectsVue.vue
+++ src/components/sections/ProjectsVue.vue
@@ -1,15 +1,72 @@
 <template>
-  <div>
-    <h1>Projects</h1>
+  <div id="projects">
+    <h1 class="m-5">Projetos</h1>
+    <h4 class="m-5">
+      Conheça meus projetos pessoais, onde aplico o conhecimento adquirido em
+      prática
+    </h4>
+    <section class="projectArea">
+      <div class="projectDiv">
+        <img src="../../assets/projectsImg/climapp.png" alt="" />
+        <div class="explanation">
+          <h4 class="mb-4">Weather App</h4>
+          <h6 class="mb-5">
+            Aplicação de clima construida em Vue consumindo a API da
+            OpenWeatherAPI
+          </h6>
+          <button class="btn btn-secondary">Detalhes</button>
+        </div>
+      </div>
+
+      <div class="projectDiv">
+        <img src="../../assets/projectsImg/pokedex.png" alt="" />
+        <div class="explanation">
+          <h4 class="mb-4">Pokedex</h4>
+          <h6 class="mb-5">
+            Pokedex completa construida em Vue consumindo a API da PokeAPI
+          </h6>
+          <button class="btn btn-secondary">Detalhes</button>
+        </div>
+      </div>
+
+      <div class="projectDiv">
+        <img src="../../assets/projectsImg/hipsta.png" alt="" />
+        <div class="explanation">
+          <h4 class="mb-4">Jogo Web - Hipsta</h4>
+          <h6 class="mb-5">
+            Jogo Endless Runner construido durante a Imersão GameDev da Alura
+          </h6>
+          <button class="btn btn-secondary">Detalhes</button>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.projectArea {
+  display: flex;
+  flex-direction: column;
+  margin: 0 1.5em;
 
+  .projectDiv {
+    display: flex;
+    justify-content: space-evenly;
+    align-items: center;
+    padding: 3rem;
+
+    img {
+      width: 30%;
+      margin: 0 auto;
+    }
+
+    .explanation {
+      padding: 2rem;
+    }
+  }
+}
 </style>
